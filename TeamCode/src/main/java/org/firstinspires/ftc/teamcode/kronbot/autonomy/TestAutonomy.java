943780b3dda0c0a7b669ee69736ec48fe642786f
AlexDavid26 <82370626+AlexDavid26@users.noreply.github.com>
2024-01-09 14:49:19 +0200

    caca

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/kronbot/autonomy/TestAutonomy.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/kronbot/autonomy/TestAutonomy.java
@@ -1,38 +1,49 @@
 package org.firstinspires.ftc.teamcode.kronbot.autonomy;
 
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Trajectory;
+import com.acmerobotics.roadrunner.TrajectoryActionBuilder;
+import com.acmerobotics.roadrunner.TrajectoryBuilder;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
 import org.firstinspires.ftc.teamcode.kronbot.KronBot;
 import org.firstinspires.ftc.teamcode.kronbot.utils.Constants;
+import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
 
 @Autonomous(name = "Test Autonomy", group = Constants.TEST_GROUP)
 public class TestAutonomy extends LinearOpMode {
     private final KronBot robot = new KronBot();
+    private MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0,0,0));
+
+    private class MoveAction implements Action {
+
+        @Override
+        public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+            drive.actionBuilder(drive.pose)
+                    .splineToConstantHeading(new Vector2d(0,10),drive.pose.heading)
+                    .build();
+            return true;
+        }
+    }
 
     @Override
     public void runOpMode() throws InterruptedException {
         robot.init(hardwareMap);
+        MecanumDrive drive = new MecanumDrive(hardwareMap, new Pose2d(0,0,0));
 
-//        SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
-//        drive.setPoseEstimate(new Pose2d(0, 0, 0));
-//
-//        TrajectoryVelocityConstraint velCon = SampleMecanumDrive.getVelocityConstraint(DriveConstants.MAX_VEL / 5, DriveConstants.MAX_ANG_VEL, DriveConstants.TRACK_WIDTH);
-//        TrajectoryVelocityConstraint angCon = SampleMecanumDrive.getVelocityConstraint(DriveConstants.MAX_VEL, DriveConstants.MAX_ANG_VEL * 1.5, DriveConstants.TRACK_WIDTH);
-//
-//        TrajectorySequence trajectory = drive.trajectorySequenceBuilder(drive.getPoseEstimate())
-//            .forward(55.6)
-//            .build();
 
+        Actions.runBlocking(new MoveAction());
         waitForStart();
 
         while (!isStopRequested() && opModeIsActive()) {
             telemetry.update();
         }
-
-//        drive.followTrajectorySequenceAsync(trajectory);
-//
-//        while (drive.isBusy())
-//            drive.update();
     }
 }
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/kronbot/utils/drivers/ActionDriver.java
@@ -0,0 +1,23 @@
+package org.firstinspires.ftc.teamcode.kronbot.utils.drivers;
+
+import androidx.annotation.NonNull;
+
+import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.kronbot.KronBot;
+import org.firstinspires.ftc.teamcode.roadrunner.MecanumDrive;
+
+public class ActionDriver implements Action{
+    MecanumDrive drive = new MecanumDrive()
+    @Override
+    public boolean run(@NonNull TelemetryPacket telemetryPacket) {
+        drive.actionBuilder(drive.pose)
+                .splineToConstantHeading(new Vector2d(0,10),drive.pose.heading)
+                .build();
+        return true;
+    }
+}
