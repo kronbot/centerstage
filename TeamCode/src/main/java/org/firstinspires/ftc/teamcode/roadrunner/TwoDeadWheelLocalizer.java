a72ffcc9a5d4b05f96967e14d1771fe3f9b70cd0
AlexMoto69 <alexmotoasca2006@gmail.com>
2024-01-13 22:52:58 +0200

    math bad, shower thought

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/TwoDeadWheelLocalizer.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/roadrunner/TwoDeadWheelLocalizer.java
@@ -21,8 +21,8 @@ import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 @Config
 public final class TwoDeadWheelLocalizer implements Localizer {
     public static class Params {
-        public double parYTicks = 0.0015696073199; // y position of the parallel encoder (in tick units)
-        public double perpXTicks = 0.0012848338918; // x position of the perpendicular encoder (in tick units)
+        public double parYTicks = 4493.1868976193; // y position of the parallel encoder (in tick units)
+        public double perpXTicks = 3960.9103660221; // x position of the perpendicular encoder (in tick units)
     }
 
     public static Params PARAMS = new Params();
